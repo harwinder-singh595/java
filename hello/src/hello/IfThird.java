dbde21a8117dc748ae77f192c969e6dae6aa8c79
1992661 <1992661@E20736.CEGEPGIM.LCL>
2019-07-11 14:03:13 -0400

    first commit from eclipse

---
+++ hello/src/hello/IfThird.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class IfThird {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hello/src/hello/MADZoo.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class MADZoo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+system.out.println("hello");
+	}
+
+}
